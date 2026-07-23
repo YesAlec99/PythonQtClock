823689fae31913cbc7170cbe0b3f7d9854663766
Alec Bosman <bosmanalec1@gmail.com>
2026-07-23 22:11:00 +0200

    Got basics done. QT design is somewhat done.Need to edit quite a bit. Commit contains file in which basics of qt is in. Will remove and edit as needed

---
+++ PyQt_Clock Start.py
@@ -0,0 +1,36 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(200, 200, 300, 300)
+        self.setWindowTitle("Timer")
+
+        self.initUI()
+    def initUI(self):
+        self.b1 = QtWidgets.QPushButton(self)
+        self.b1.setText("Click me")
+        self.b1.clicked.connect(self.clicked)
+
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("Timer for studying ")
+        self.label.move(50, 50)
+    def clicked(self):
+        self.label.setText("Your pressed the button ")
+        self.update()
+    def update(self):
+        self.label.adjustSize()
+
+def clicked() :
+    print("Clicked")
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+window()
